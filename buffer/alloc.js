1c017923f01a24a3c24143dc050815eb50b4b71d
felixovo <felix2022daily@qq.com>
2023-10-22 22:52:50 +0800

    update

--- buffer/alloc.js
+++ buffer/alloc.js
@@ -1,9 +1,13 @@
 let buf = Buffer.alloc(10)
 
-buf.write("abcdefghijkl")
+let str = "abcdefghijkl"
+buf.write(str)
 
 console.log(buf)
 // <Buffer 61 62 63 64 65 66 67 68 69 6a>
+
+console.log(buf.length)
+// 10
 
 console.log(buf.toString())
 // "abcdefghij"
